3bc881b88f7c6a615235ce25ff0c1902e8134207
Vennela <eruvalasrivennela8@gmail.com>
2024-05-28 20:47:28 +0530

    updated package.json

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const transactions = require('./routes/transactions');
 app.use('/api/v1/transactions',transactions);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
+    app.use("/",express.static('client/build'));
 
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
